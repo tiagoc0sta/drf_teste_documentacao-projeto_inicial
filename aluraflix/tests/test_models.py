26d16d2ef57d41201088b45151307281680e271d
Tiago Costa <tiagodpv@gmail.com>
2023-08-04 18:59:40 -0400

    fith

--- aluraflix/tests/test_models.py
+++ aluraflix/tests/test_models.py
@@ -5,14 +5,14 @@ class PrgramaModelTestCase(TestCase):
 
     def setUp(self):
       self.programa = Programa(
-        titulo = ' Procurando ninguem em latim ',
+        titulo = 'Procurando ninguem em latim',
         data_lancamento = '2003-07-04'
       )
       
     def test_verifica_atributos_do_programa(self):
       """Teste que verifica os atributos de um programa com valores default"""
-      self.assertEqual(self.programa.titulo, 'Procurando ninguém em latim')
+      self.assertEqual(self.programa.titulo, 'Procurando ninguem em latim')
       self.assertEqual(self.programa.tipo, 'F')
       self.assertEqual(self.programa.data_lancamento, '2003-07-04')
-      self.assertEqual(self.programa.LIKES, 0)
+      self.assertEqual(self.programa.likes, 0)
       self.assertEqual(self.programa.dislikes, 0)
